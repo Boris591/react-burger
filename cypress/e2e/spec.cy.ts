3ef88bacfa420dad1f3d6af30a9fe06934c5b6f2
boriskrivolapov <boris.kr07@gmail.com>
2023-07-30 23:52:22 +0300

    test cypress

---
+++ cypress/e2e/spec.cy.ts
@@ -0,0 +1,34 @@
+describe("Test", () => {
+  beforeEach(() => {
+    cy.visit('/');
+  });
+
+  it('Test Ingredient Card', function () {
+    cy.get('[class^=ingredient-card_card]').first()
+        .should('exist');
+    cy.get('[class^=ingredient-card_card]').first().click();
+    cy.get('[class^=modal_modal]').contains('Детали ингридиента').should('exist');
+    cy.get('[class^=modal_close]').click();
+  });
+
+  it('Test create order', function () {
+    cy.get('[class^=ingredient-card_card]').first()
+        .should('exist');
+
+    cy.get('[class^=ingredient-card_card]').first().trigger('dragstart');
+    cy.get('[class^=burger-constructor_list]').trigger('drop');
+
+    cy.get('[class^=constructor-card_main]').first()
+        .should('exist');
+    cy.get('button').contains('Оформить заказ').click();
+    cy.get('input[name="email"]').type('boris.kr07@gmail.com');
+    cy.get('input[name="password"]').type('bubacho123Z5');
+    cy.get('button').contains('Войти').click();
+    cy.get('button').contains('Оформить заказ').click();
+    cy.wait(20 * 1000);
+    cy.get('[class^=modal_modal]').contains('идентификатор заказа').should('exist');
+  });
+
+});
+
+
